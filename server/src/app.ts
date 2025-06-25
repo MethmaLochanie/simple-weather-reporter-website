c7655a54ccaa20aa01548b7d2790552d21bf4e44
Methuu1119 <104524747+MethmaLochanie@users.noreply.github.com>
2025-06-26 01:14:40 +0530

    Fix : cors allow origin issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,6 +13,7 @@ const corsOptions = {
     if (!origin) return callback(null, true);
     
     const allowedOrigins = [
+      'https://simple-weather-reporter-website.vercel.app',
       'http://localhost:5173',
       'http://localhost:3000',
       'http://127.0.0.1:5173',
